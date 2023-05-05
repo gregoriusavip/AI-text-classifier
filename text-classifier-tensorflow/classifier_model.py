7c1547a68ddecef1957e3f3d2ed5be5a5ba6dc4f
Gregorius Avip <71865513+gavip1@users.noreply.github.com>
2023-05-04 22:03:18 -0700

    Add files via upload

---
+++ text-classifier-tensorflow/classifier_model.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import os
+import numpy as np
+
+cwd = os.getcwd()
+relative_path = os.path.join("text_classification_model")
+
+absolute_path = os.path.abspath(os.path.join(cwd, relative_path))
+print(absolute_path)
+loaded_model = tf.keras.models.load_model(absolute_path)
+
+sample_text = ('The movie was not good. The animation and the graphics '
+               'were terrible. I would not recommend this movie.')
+predictions = loaded_model.predict(np.array([sample_text]))
+print(predictions)
---
+++ text-classifier-tensorflow/classifier_website.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+def my_function(user_input):
+   # Your Python script logic here
+   output = "Inputted text is: " + user_input
+   return output
+
+@app.route('/')
+def home():
+   return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+   user_input = request.form['user_input']
+   output = my_function(user_input)
+   return render_template('output.html', output=output)
+
+if __name__ == '__main__':
+   app.run(debug=True)
