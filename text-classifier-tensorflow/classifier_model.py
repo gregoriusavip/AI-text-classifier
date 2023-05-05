a4ca0f1749090118ed438805a5f8206b6f2fa5f2
Gregorius Avip <71865513+gavip1@users.noreply.github.com>
2023-05-04 22:09:53 -0700

    fix keras_metadata.pb

--- text-classifier-tensorflow/classifier_model.py
+++ text-classifier-tensorflow/classifier_model.py
@@ -9,7 +9,6 @@ absolute_path = os.path.abspath(os.path.join(cwd, relative_path))
 print(absolute_path)
 loaded_model = tf.keras.models.load_model(absolute_path)
 
-sample_text = ('The movie was not good. The animation and the graphics '
-               'were terrible. I would not recommend this movie.')
+sample_text = "The movie was not good. The animation and the graphics 'were terrible. I would not recommend this movie."
 predictions = loaded_model.predict(np.array([sample_text]))
 print(predictions)
