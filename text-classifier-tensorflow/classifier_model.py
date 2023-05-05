fbf4699194f179ec1960ae909211330a6bb41f68
Gregorius Avip <71865513+gavip1@users.noreply.github.com>
2023-05-04 23:06:24 -0700

    Update classifier_model.py

--- text-classifier-tensorflow/classifier_model.py
+++ text-classifier-tensorflow/classifier_model.py
@@ -6,12 +6,11 @@ class text_model:
     cwd = os.getcwd()
     relative_path = os.path.join("text_classification_model")
     absolute_path = os.path.abspath(os.path.join(cwd, relative_path))
-    print(absolute_path)
+    #If the file is not found, make sure that the absolute path is the path of text_classification_model folder
     loaded_model = tf.keras.models.load_model(absolute_path)
 
     def predict_text(self, input_text):
         predictions = self.loaded_model.predict(np.array([input_text]))
         return predictions
-
 
 model = text_model()
