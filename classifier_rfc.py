7285aaa69f59d3491e9064099612d69cb19c7af3
Gregorius Avip <gregorius.avip@gmail.com>
2023-09-22 00:37:06 -0700

    Move files & remove tensorflow model
    the folder text-classifier-tensorflow exist twice for no reason and therefore was removed.
    the tensorflow model itself is removed because of bugged interaction when trying to load the model.

---
+++ classifier_rfc.py
@@ -0,0 +1,18 @@
+import joblib
+import os
+
+class rfc_model():
+    def __init__(self):
+        cwd = os.getcwd()
+        rfc_filepath = os.path.join(cwd, "rfc_model", "rfc_sentiment_model.joblib")
+        rfc_vectorizer = os.path.join(cwd, "rfc_model", "rfc_vectorizer.joblib")
+        rfc_encoder = os.path.join(cwd, "rfc_model", "rfc_encoder.joblib")
+
+        self.rfc = joblib.load(rfc_filepath)
+        self.vectorizer = joblib.load(rfc_vectorizer)
+        self.encoder = joblib.load(rfc_encoder)
+    
+    def predict(self, text):
+        X_new_counts = self.vectorizer.transform([text])
+        predicted = self.rfc.predict(X_new_counts)
+        return self.encoder.inverse_transform(predicted)[0]
---
+++ classifier_website.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+from classifier_rfc import rfc_model
+
+random_forest_model = rfc_model()
+app = Flask(__name__)
+
+def my_function(user_input):
+   output = []
+   output.append("using Random Forest model based of twitter comment dataset: " + random_forest_model.predict(user_input))
+   
+   return output
+
+@app.route('/')
+def home():
+   return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+   user_input = "The text: "
+   user_input += "<b>" + request.form['user_input'] + "</b>"
+   output = my_function(user_input)
+   return render_template('output.html', output=output, user_input=user_input)
+
+if __name__ == '__main__':
+   app.run(debug=True)
