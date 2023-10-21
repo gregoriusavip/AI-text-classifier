45f2f546347e704d54a070bcb9a591bf3d33d2d1
Gregorius Avip <gregorius.avip@gmail.com>
2023-10-21 00:45:05 -0700

    Move Flask apps to folder backend

---
+++ backend/model_loader/classifier_rfc_loader.py
@@ -0,0 +1,16 @@
+import joblib
+import os
+
+class rfc_model():
+    def __init__(self):
+        cwd = os.getcwd()
+        rfc_filepath = os.path.join(cwd, "backend", "rfc_model", "rfc_sentiment_model.joblib")
+        rfc_vectorizer = os.path.join(cwd, "backend", "rfc_model", "rfc_vectorizer.joblib")
+
+        self.rfc = joblib.load(rfc_filepath)
+        self.vectorizer = joblib.load(rfc_vectorizer)
+    
+    def predict(self, text):
+        X_new_counts = self.vectorizer.transform([text])
+        predicted = self.rfc.predict(X_new_counts)
+        return predicted[0]
